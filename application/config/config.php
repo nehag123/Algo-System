64ff41bb28c87cf0ee8876a05190f89fbc908946
Neha Gulati <neha.gulati@kindlebit.com>
2019-02-12 17:50:20 +0530

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://algo.systems/';
+$config['base_url'] = 'https://algo.systems/index.php/';
 
 /*
 |--------------------------------------------------------------------------
